acd9b4fb43617969045a9062f799e92baf266456
OctopiWithHats <sdgranoff@gmail.com>
2020-03-26 20:30:48 -0400

    ugly but working (and part one of I can't believe it took this long  of fighting with SourceTree to figure this out)

---
+++ Morning Ritual/Assets/Scripts/RandomScene.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomScene : MonoBehaviour
+{
+    [SerializeField]
+    private string[] sceneNames;
+
+    private string chosenScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        chosenScene = sceneNames[Random.Range(0, sceneNames.Length)];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(chosenScene);
+    }
+
+
+    static public void LoadSceneNamed(string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
